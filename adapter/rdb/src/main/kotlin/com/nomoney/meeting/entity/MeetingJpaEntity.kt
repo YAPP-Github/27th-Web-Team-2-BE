b36e32f7ab5b6f885d598052799767f9bddf1e8a
comforest <comforest@naver.com>
2026-01-11 14:32:44 +0900

    14 style: ktlint

--- adapter/rdb/src/main/kotlin/com/nomoney/meeting/entity/MeetingJpaEntity.kt
+++ adapter/rdb/src/main/kotlin/com/nomoney/meeting/entity/MeetingJpaEntity.kt
@@ -1,7 +1,12 @@
 package com.nomoney.meeting.entity
 
 import com.nomoney.base.BaseJpaEntity
-import jakarta.persistence.*
+import jakarta.persistence.CascadeType
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
+import jakarta.persistence.Table
 
 @Entity
 @Table(name = "meetings")
