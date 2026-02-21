a837e613f1e237e1776f9ff4bf6dbc512df007f8
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2026-02-21 15:35:20 +0900

    62 feat: Swagger Operation 문자열 하드코딩 중앙관리 리팩터링

--- app/api/src/main/kotlin/com/nomoney/api/swagger/SwaggerApiTag.kt
+++ app/api/src/main/kotlin/com/nomoney/api/swagger/SwaggerApiTag.kt
@@ -3,4 +3,9 @@ package com.nomoney.api.swagger
 object SwaggerApiTag {
     const val TEST = "[TEST] 00. Test Api / 운영에서는 제공하지 않음"
     const val MEET_VOTE = "[API] 01. 일정 투표 관련"
+    const val AUTH = "인증"
+    const val AUTH_DESCRIPTION = "인증 관련 API"
+    const val MEETING_QUERY_CREATE = "모임 조회/생성 API"
+    const val HOST_MEETING_MANAGEMENT = "주최자 모임 관리 API"
+    const val VOTE = "투표 API"
 }
