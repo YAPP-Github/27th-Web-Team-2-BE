c5d7e9913538eb9c5438ad4a64928205644c970b
SANGMIN PARK <71458064+pp8817@users.noreply.github.com>
2026-03-23 14:38:18 +0900

    86 style: ktlint 오류 수정

--- app/api/src/test/kotlin/com/nomoney/api/auth/TokenAuthenticationFilterTest.kt
+++ app/api/src/test/kotlin/com/nomoney/api/auth/TokenAuthenticationFilterTest.kt
@@ -103,4 +103,4 @@ class TokenAuthenticationFilterTest : DescribeSpec({
             }
         }
     }
-})
+},)
--- support/logging/src/main/kotlin/com/nomoney/support/logging/ApiLoggingFilter.kt
+++ support/logging/src/main/kotlin/com/nomoney/support/logging/ApiLoggingFilter.kt
@@ -7,9 +7,9 @@ import jakarta.servlet.ServletRequest
 import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import org.springframework.http.HttpStatus
-import org.slf4j.MDC
 import org.slf4j.LoggerFactory
+import org.slf4j.MDC
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.util.ContentCachingRequestWrapper
 import org.springframework.web.util.ContentCachingResponseWrapper
